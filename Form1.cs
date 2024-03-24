8baa68425f3835b6260f1bdfab7bd9b8049b38f5
Macksn <itsmacksn@yahoo.com>
2024-03-24 21:47:57 +0300

    Not_Hesabi
    Class ile not hesabı

---
+++ Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace not
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        not eleman = new not();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            eleman.not1 = Convert.ToDouble(textBox1.Text);
+            eleman.not2 = Convert.ToDouble(textBox2.Text);
+            eleman.not3 = Convert.ToDouble(textBox3.Text);
+            label1.Text = eleman.ortalama().ToString();
+            label2.Text = eleman.sonuc();
+        }
+    }
+}
---
+++ not.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace not
+{
+    class not
+    {
+        public double not1, not2, not3, ort;
+        string son;
+        public double ortalama()
+        {
+            ort = (not1 + not2 + not3) / 3;
+            return (ort);
+        }
+        public string sonuc()
+        {
+            if (ort < 50)
+                son = "kaldı";
+            else
+                son = "gecti";
+            return (son);
+        }
+    }
+}
